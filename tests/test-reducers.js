7ef5b012fdf8bc6af090a9411dd3f5c30fc11788
kkindorf <kevin.kindorf@gmail.com>
2016-09-24 19:04:59 +0000

    added tests for reducer

---
+++ tests/test-reducers.js
@@ -0,0 +1,74 @@
+import expect from 'expect';
+var actions = require('../actions');
+
+
+var appReducer = require('../reducers').appReducer;
+
+describe('app reducer', function(){
+    
+    it('should return the initial state', function(){
+        expect(appReducer(undefined, {})).toEqual({
+            tumblrDataResponse: [],
+        	postedData: {},
+	        dbData: []
+        })
+   
+    })
+    it('should fetch tumblr data', function(){
+        expect(appReducer({}, {
+           type: actions.FETCH_TUMBLR_SUCCESS,
+           tumblrDataResponse: []
+        })).toEqual({
+	        tumblrDataResponse: []
+        })
+    })
+    it('should post data to the database', function(){
+        expect(appReducer({},{
+            type: actions.POST_TUMBLR_SUCCESS,
+            postedData: {
+                src: 'this is a src',
+                blogName: 'this is a blogName',
+                postUrl: 'this is a postUrl',
+                summary: 'this is a summary'
+            }
+        })).toEqual({
+            postedData:{
+                src: 'this is a src',
+                blogName: 'this is a blogName',
+                postUrl: 'this is a postUrl',
+                summary: 'this is a summary'
+            },
+
+        })
+    })
+    it('should fetch posts from db success', function(){
+        expect(appReducer({},{
+            type: actions.FETCH_POSTS_FROM_DB_SUCCESS,
+            dbData: [{
+                src: 'this is a src',
+                blogName: 'this is a blogName',
+                postUrl: 'this is a postUrl',
+                summary: 'this is a summary'
+            },
+            {
+                src: 'this is a src 2',
+                blogName: 'this is a blogName 2',
+                postUrl: 'this is a postUrl 2',
+                summary: 'this is a summary 2'
+            }]
+        })).toEqual({
+            dbData: [{
+                src: 'this is a src',
+                blogName: 'this is a blogName',
+                postUrl: 'this is a postUrl',
+                summary: 'this is a summary'
+            },
+            {
+                src: 'this is a src 2',
+                blogName: 'this is a blogName 2',
+                postUrl: 'this is a postUrl 2',
+                summary: 'this is a summary 2'
+            }]
+        })
+    })
+})
